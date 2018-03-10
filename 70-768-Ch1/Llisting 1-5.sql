74af17fbb914ba375c2ebfbb673006cd91b6d9c3
Phillip Raywood <phillipraywood@icloud.com>
2018-03-10 09:55:28 -0500

    Skill 1.2: Design and implement dimensions in a cube

---
+++ 70-768-Ch1/Llisting 1-5.sql
@@ -0,0 +1,11 @@
+SELECT
+   1 SalesReasonKey
+  ,'Value' SalesReason
+UNION
+SELECT
+   2 SalesReasonKey
+  ,'Manufacturer' SalesReason
+UNION
+SELECT
+   3 SalesReasonKey
+  ,'Sales Person' SalesReason;
---
+++ 70-768-Ch1/Llisting 1-6.sql
@@ -0,0 +1,15 @@
+SELECT
+   CAST( 1 AS bigint ) [Sale Key]
+  ,1 SalesReasonKey
+UNION
+SELECT
+   CAST( 1 AS bigint ) [Sale Key]
+  ,2 SalesReasonKey
+UNION
+SELECT
+   CAST( 1 AS bigint ) [Sale Key]
+  ,3 SalesReasonKey
+UNION
+SELECT
+   CAST( 2 AS bigint ) [Sale Key]
+  ,1 SalesReasonKey;
